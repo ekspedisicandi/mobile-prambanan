813269e49b27aac805cea5d75efb1d5111942a90
faridiot <defaultride.bruno@gmail.com>
2015-05-06 23:24:49 +0700

    faridiot

---
+++ scripts/cctv.js
@@ -0,0 +1,41 @@
+/* 
+!! Unstable at wide resolutions!!
+
+If stuff starts disapearing try scaling your browser, this piece is a bit delicate and resource hungry :)
+
+Increase height of viewable area to see more trees.
+Trees chopped for slim viewing.
+
+For best viewing results, full view and scale the browser.
+
+*/
+
+
+
+
+
+/* camera timer */
+var minutesLabel = document.getElementById("minutes");
+        var secondsLabel = document.getElementById("seconds");
+        var totalSeconds = 0;
+        setInterval(setTime, 1000);
+
+        function setTime()
+        {
+            ++totalSeconds;
+            secondsLabel.innerHTML = pad(totalSeconds%60);
+            minutesLabel.innerHTML = pad(parseInt(totalSeconds/60));
+        }
+
+        function pad(val)
+        {
+            var valString = val + "";
+            if(valString.length < 2)
+            {
+                return "0" + valString;
+            }
+            else
+            {
+                return valString;
+            }
+        }
